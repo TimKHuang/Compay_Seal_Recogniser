b50a4111bf8c0ed2141006975b3c783c3abe9054
TimKHuang <timkhuang@icloud.com>
2020-06-19 08:40:15 +0800

    first try on comparing two seals

--- src/python/main.py
+++ src/python/main.py
@@ -2,5 +2,4 @@ from ellipse_detector import EllipseDetector
 from seal_comparator import SealComparator
 
 if __name__ == "__main__":
-    for ellipse in EllipseDetector()('images/source/test.png', 'images/output/'):
-        SealComparator()(ellipse)
+    EllipseDetector()('images/source/test.png')
--- src/python/seal_comparator.py
+++ src/python/seal_comparator.py
@@ -11,10 +11,27 @@ class SealComparator():
     value_low = 5
     value_high = 255
 
-    def __call__(self, seal):
+    def __call__(self, seal_collection):
+        processed_seal_collection = []
+        for seal in seal_collection:
+            processed_seal_collection.append(self.preprocess(seal))
+            cv2.imshow('test', seal)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        for i in range(len(processed_seal_collection) - 1 ):
+            for m in range(i + 1, len(processed_seal_collection)):
+                image1 = processed_seal_collection[i]
+                image2 = processed_seal_collection[m]
+                ncc = self.calculate_ncc(image1, image2)
+                cv2.imwrite('images/output/' + str(ncc) + '.png', image1)
+                cv2.imwrite('images/output/' + str(ncc) + '.jpg', image2)
+                cv2.destroyAllWindows()
+
+    def preprocess(self, seal):
         binary_image = self.threshold(seal)
         noise_reduced_image = self.reduce_noise(binary_image)
         fixed_image = self.close_calculate(noise_reduced_image)
+        return fixed_image
 
     def threshold(self, image):
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -37,3 +54,17 @@ class SealComparator():
     def close_calculate(self, image):
         kerenal = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         return cv2.morphologyEx(image, cv2.MORPH_CLOSE, kerenal)
+    
+    def calculate_ncc(self, image1, image2):
+        (height1, _) = image1.shape
+        (height2, _) = image2.shape
+        if height1 < height2:
+            image2 = self.resize(image2, height1)
+        else:
+            image1 = self.resize(image1, height2)
+    
+        covariance = np.mean(np.multiply( (image1 - np.mean(image1,)), (image2 - np.mean(image2)) ))
+        return covariance / ( np.std(image1) * np.std(image2) )
+
+    def resize(self,image,  side_length):
+        return cv2.resize(image, (side_length, side_length))
